1b9dadc1e5e40292dce7419e0ebac6ffc53a3bda
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2023-05-24 22:27:25 +0200

    adding the transaprent

--- components/Navbar.js
+++ components/Navbar.js
@@ -54,35 +54,36 @@ const scrollToHome = (router) => {
   return (
     <>
 
-   <div className=" md:z-50 md:w-full md:h-16 md:max-w-lg md:-translate-x-1/2 md:bg-[#202020] md:border  md:rounded-md md:border-transparent  md:bottom-4 md:left-1/2   fixed bottom-0 left-0 z-50 w-full h-16 bg-[#202020] border-t border-bg-[#202020] dark:bg-bg-[#202020] ">
-    <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
-        <button type="button" className="inline-flex md:border-r font-bold  md:uppercase md:text-xl  flex-col items-center justify-center px-8   hover:bg-black  hover:text-white">
-            <svg className="md:hidden w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-            </svg>
-            <Link className="text-md text-gray-500 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white uppercase" href="/" onClick={() => scrollToHome(router)}>chester  </Link>
+<div className="md:z-50 md:w-full md:h-16 md:max-w-lg md:-translate-x-1/2 md:border-2 border-2 md:rounded-md md:border-transparent md:bottom-4 md:left-1/2 fixed bottom-0 left-0 z-50 w-full h-16 border-t backdrop-filter backdrop-blur-sm dark:backdrop-blur-sm">
+  <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
+    <button type="button" className="inline-flex md:border-r font-bold md:uppercase md:text-xl flex-col items-center justify-center px-8 hover:bg-black hover:text-white">
+      <svg className="md:hidden w-6 h-6 mb-1 text-white group-hover:text-white dark:group-hover:text-black" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+      </svg>
+      <Link className="text-md text-white group-hover:text-white dark:group-hover:text-black uppercase" href="/" onClick={() => scrollToHome(router)}>chester</Link>
+    </button>
+    <button type="button" className="inline-flex md:border-r-2 md:uppercase md:text-lg flex-col items-center justify-center px-5 border-gray-200 hover:bg-black ">
+      <svg className="md:hidden w-6 h-6 mb-1 text-white group-hover:text-white dark:group-hover:text-black" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path d="M10 1a9 9 0 100 18 9 9 0 000-18zM7 7a1 1 0 011-1h4a1 1 0 010 2H8a1 1 0 01-1-1zm0 4a1 1 0 011-1h4a1 1 0 010 2H8a1 1 0 01-1-1zm0 4a1 1 0 011-1h4a1 1 0 010 2H8a1 1 0 01-1-1zm9-6a1 1 0 011 1v4a1 1 0 01-2 0V7a1 1 0 011-1zm-4-1a1 1 0 00-1 1v4a1 1 0 102 0V7a1 1 0 00-1-1z"></path>
+      </svg>
+      <span className="text-sm text-white group-hover:text-white dark:group-hover:text-black" href="/" onClick={() => scrollToExperience(router)}>Experience</span>
+    </button>
+    <button type="button" className="inline-flex md:border-r md:uppercase md:text-lg flex-col items-center justify-center px-5 hover:bg-black">
+      <svg className="md:hidden w-6 h-6 mb-1 text-white group-hover:text-white dark:group-hover:text-black" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
+      </svg>
+            <span className="text-sm text-white  group-hover:text-white dark:group-hover:text-black"  href="/" onClick={() =>scrollToWork(router)}>My Work </span>
         </button>
-        <button type="button" className="inline-flex md:border-r-2  md:uppercase md:text-lg flex-col items-center justify-center px-5  border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-800 group dark:border-gray-600">
-        <svg className="md:hidden w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <path d="M10 1a9 9 0 100 18 9 9 0 000-18zM7 7a1 1 0 011-1h4a1 1 0 010 2H8a1 1 0 01-1-1zm0 4a1 1 0 011-1h4a1 1 0 010 2H8a1 1 0 01-1-1zm0 4a1 1 0 011-1h4a1 1 0 010 2H8a1 1 0 01-1-1z"></path>
-        </svg>
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" href="/" onClick={() =>scrollToExperience(router)}>Experience</span>
-        </button>
-        <button type="button" className="inline-flex md:border-r md:uppercase md:text-lg flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-            <svg className="md:hidden w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
-            </svg>
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"  href="/" onClick={() =>scrollToWork(router)}>My Work </span>
-        </button>
-        <button type="button" className="inline-flex  md:uppercase md:text-lg flex-col items-center justify-center px-5 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-800 group  dark:border-gray-600">
-            <svg className="md:hidden w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <button type="button" className="inline-flex  md:uppercase md:text-lg flex-col items-center justify-center px-5 border-gray-200 hover:bg-black  dark:border-black">
+            <svg className="md:hidden w-6 h-6 mb-1 text-white  group-hover:text-white dark:group-hover:text-black" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"></path>
             </svg>
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" href="/" onClick={() =>scrollToAbout(router)}>About Me  </span>
+            <span className="text-sm text-white  group-hover:text-white dark:group-hover:text-black" href="/" onClick={() =>scrollToAbout(router)}>About Me  </span>
         </button>
     </div>
 
 </div>
+
  <main>
         { children }
  </main>
--- components/Work.js
+++ components/Work.js
@@ -19,21 +19,21 @@ const Experiance = () => {
   <h3 className="text-sm px-4 md:text-3xl justify-end text-right">Some opportunities and projects <br /> that I’m proud of! </h3>
 </div>
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-2 gap-4">
-    <div className="bg-white rounded-lg shadow-lg">
+    <div className="bg-black rounded-lg shadow-lg">
  {/* eslint-disable no-alert, no-console */}
     <img  src="https://user-images.githubusercontent.com/69056906/226124010-b79b0763-8c33-4776-b70a-53f65d80a1ae.png" alt="Project 1" className="w-full h-48 object-cover rounded-t-lg" />
     <div className="p-4">
       <h3 className="text-xl font-bold mb-2">News</h3>
-      <p className="text-gray-600 mb-4">is a news and content platform that covers a wide range of topics such as current events, politics, sports, and lifestyle.</p>
+      <p className="text-white mb-4 text-md font-extralight">is a news and content platform that covers a wide range of topics such as current events, politics, sports, and lifestyle.</p>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 px-2 gap-4">
-      <a href="https://feed-news.netlify.app/" className="flex items-center text-purple-600">
+      <a href="https://feed-news.netlify.app/" className="flex items-center text-purple-400 hover:underline">
         Go Live
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
         </svg>
       </a>
 
-      <a href="https://github.com/rotshidzw/peca" className="flex items-center text-purple-600">
+      <a href="https://github.com/rotshidzw/peca" className="flex items-center text-purple-400 hover:underline">
         Git code
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
@@ -43,21 +43,21 @@ const Experiance = () => {
     </div>
   </div>
 
-  <div className="bg-white rounded-lg shadow-lg">
+  <div className="bg-black rounded-lg shadow-lg">
  {/* eslint-disable no-alert, no-console */}
     <img src="https://user-images.githubusercontent.com/69056906/235224391-f1dc04a4-caf1-4748-85ec-937777ecceb2.png" alt="Project 2" className="w-full h-48 object-cover rounded-t-lg" />
     <div className="p-4">
       <h3 className="text-xl font-bold mb-2">The-Crystal-Project</h3>
-      <p className="text-gray-600 mb-4">The template also includes pages for services, pricing, and booking, as well</p>
+      <p className="text-white mb-4 text-md font-extralight">The template also includes pages for services, pricing, and booking, as well</p>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 px-2 gap-4">
-      <a href="https://storied-sunburst-99dd6f.netlify.app/" className="flex items-center text-purple-600">
+      <a href="https://storied-sunburst-99dd6f.netlify.app/" className="flex items-center text-purple-400 hover:underline">
         Go Live
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
         </svg>
       </a>
 
-      <a href="https://github.com/rotshidzw/the-crystal-project" className="flex items-center text-purple-600">
+      <a href="https://github.com/rotshidzw/the-crystal-project" className="flex items-center text-purple-400 hover:underline">
         Git code
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
@@ -66,21 +66,21 @@ const Experiance = () => {
       </div>
     </div>
   </div>
-  <div className="bg-white rounded-lg shadow-lg">
+  <div className="bg-black rounded-lg shadow-lg">
  {/* eslint-disable no-alert, no-console */}
     <img src="https://user-images.githubusercontent.com/69056906/235226287-4ca825bc-7629-48d6-a26e-7996d0cd904f.png" alt="Project 2" className="w-full h-48 object-cover rounded-t-lg" />
     <div className="p-4">
       <h3 className="text-xl font-bold mb-2">Landing page</h3>
-      <p className="text-gray-600 mb-4"> functionality for visitors who want to Go Live about the business</p>
+      <p className="text-white mb-4 text-md font-extralight"> functionality for visitors who want to Go Live about the business</p>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 px-2 gap-4">
-      <a href="https://main--friendly-genie-e6ae39.netlify.app/" className="flex items-center text-purple-600">
+      <a href="https://main--friendly-genie-e6ae39.netlify.app/" className="flex items-center text-purple-400 hover:underline">
         Go Live
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
         </svg>
       </a>
 
-      <a href="https://github.com/rotshidzw/product-landing-page" className="flex items-center text-purple-600">
+      <a href="https://github.com/rotshidzw/product-landing-page" className="flex items-center text-purple-400 hover:underline">
         Git code
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
@@ -89,21 +89,21 @@ const Experiance = () => {
       </div>
     </div>
   </div>
-  <div className="bg-white rounded-lg shadow-lg">
+  <div className="bg-black rounded-lg shadow-lg">
  {/* eslint-disable no-alert, no-console */}
     <img src="https://user-images.githubusercontent.com/69056906/238276480-1e5266b6-5bf9-4000-afc4-12a454e905ff.png" alt="Project 2" className="w-full h-48 object-cover rounded-t-lg" />
     <div className="p-4">
       <h3 className="text-xl font-bold mb-2"></h3>
-      <p className="text-gray-600 mb-4">A simple and interractive React App that utilises an external space X API to provide information about space rockets and missions. User can reserve rockets, join mission and these can be viewed in a the user's profile. A user can also read more about the rockets and missions.</p>
+      <p className="text-white mb-4 text-md font-extralight">A simple and interractive React App that utilises an external space X API to provide information about space rockets and missions. User can reserve rockets, join mission and these can be viewed in a the user's profile. A user can also read more about the rockets and missions.</p>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 px-2 gap-4">
-      <a href="https://space-travelers-hubs.netlify.app/" className="flex items-center text-purple-600">
+      <a href="https://space-travelers-hubs.netlify.app/" className="flex items-center text-purple-400 hover:underline">
         Go Live
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
         </svg>
       </a>
 
-      <a href="https://github.com/rotshidzw/space-travelers-hub/tree/main" className="flex items-center text-purple-600">
+      <a href="https://github.com/rotshidzw/space-travelers-hub/tree/main" className="flex items-center text-purple-400 hover:underline">
         Git code
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
@@ -112,21 +112,21 @@ const Experiance = () => {
       </div>
     </div>
   </div>
-  <div className="bg-white rounded-lg shadow-lg">
+  <div className="bg-black rounded-lg shadow-lg">
  {/* eslint-disable no-alert, no-console */}
     <img src="https://user-images.githubusercontent.com/69056906/238199732-ec4449a8-2a49-4698-b010-d80d8be0865c.png" alt="Project 2" className="w-full h-48 object-cover rounded-t-lg" />
     <div className="p-4">
       <h3 className="text-xl font-bold mb-2">Real State Agent</h3>
-      <p className="text-gray-600 mb-4">real state agent web page I’ve helped families determine their home’s worth, set the right price, and negotiate a winning deal.</p>
+      <p className="text-white mb-4 text-md font-extralight">real state agent web page I’ve helped families determine their home’s worth, set the right price, and negotiate a winning deal.</p>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 px-2 gap-4">
-      <a href="gregarious-dragon-a4fbcc.netlify.app/?" className="flex items-center text-purple-600">
+      <a href="gregarious-dragon-a4fbcc.netlify.app/?" className="flex items-center text-purple-400 hover:underline">
         Go Live
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
         </svg>
       </a>
 
-      <a href="https://github.com/rotshidzw/landingpage" className="flex items-center text-purple-600">
+      <a href="https://github.com/rotshidzw/landingpage" className="flex items-center text-purple-400 hover:underline">
         Git code
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
@@ -135,21 +135,21 @@ const Experiance = () => {
       </div>
     </div>
   </div>
-  <div className="bg-white rounded-lg shadow-lg">
+  <div className="bg-black rounded-lg shadow-lg">
  {/* eslint-disable no-alert, no-console */}
     <img src="https://user-images.githubusercontent.com/69056906/224443795-3abb62e3-b3d5-4f36-88ea-79cea142eb60.png" alt="Project 2" className="w-full h-48 object-cover rounded-t-lg" />
     <div className="p-4">
       <h3 className="text-xl font-bold mb-2">Anti Da Menace landing page</h3>
-      <p className="text-gray-600 mb-4">shows off his youtube videos and his playlist its done by using googleapi to show all the info the rapper antidamanace including the functioning contact form furthermore the about which include who he is</p>
+      <p className="text-white mb-4 text-md font-extralight">shows off his youtube videos and his playlist its done by using googleapi to show all the info the rapper antidamanace including the functioning contact form furthermore the about which include who he is</p>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 px-2 gap-4">
-      <a href="https://antidamenace.netlify.app/" className="flex items-center text-purple-600">
+      <a href="https://antidamenace.netlify.app/" className="flex items-center text-purple-400 hover:underline">
         Go Live
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
         </svg>
       </a>
 
-      <a href="https://github.com/rotshidzw/nextjsapi" className="flex items-center text-purple-600">
+      <a href="https://github.com/rotshidzw/nextjsapi" className="flex items-center text-purple-400 hover:underline">
         Git code
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 6a.75.75 0 01.75-.75h6.5a.75.75 0 01.75.75v6.5a.75.75 0 11-1.5 0V6.56L4.72 14.78a.75.75 0 11-1.06-1.06L11.44 6H6z" clipRule="evenodd" />
@@ -161,7 +161,7 @@ const Experiance = () => {
   <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {cards.slice(0, showMore ? cards.length : 6).map((card, index) => (
-          <div key={index} className="bg-white rounded-lg shadow-lg">
+          <div key={index} className="bg-black rounded-lg shadow-lg">
          {/* eslint-disable no-alert, no-console */}
             <img
               src={card.image}
@@ -170,10 +170,10 @@ const Experiance = () => {
             />
             <div className="p-4">
               <h3 className="text-xl font-bold mb-2">{card.projectName}</h3>
-              <p className="text-gray-600 mb-4">{card.description}</p>
+              <p className="text-white mb-4 text-md font-extralight">{card.description}</p>
               <a
                 href={card.link}
-                className="flex items-center text-purple-600"
+                className="flex items-center text-purple-400 hover:underline"
               >
                 Learn More
                 <svg
@@ -206,7 +206,7 @@ const Experiance = () => {
     </div>
 </div>
 <div className="flex  justify-center items-center ">
-      <div className="flex mt-12 justify-center items-center h-[100px] w-[100px] md:h-32 md:w-32 text-white border-2   hover:text-black hover:bg-white rounded-full bg-black cursor-pointer">
+      <div className="flex mt-12 justify-center items-center h-[100px] w-[100px] md:h-32 md:w-32 text-white border-2   hover:text-black hover:bg-black rounded-full bg-black cursor-pointer">
         <Link href="/work" className="text-center text-sm md:text-md flex font-bold justify-center rounded-full">
           More Projects
           <svg
